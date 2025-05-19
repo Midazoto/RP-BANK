237a3edf150cf60514080d5fc3ddf47b5e5fba27
Nathan Le Fol <nathanlefol@hotmail.fr>
2025-05-19 14:51:12 +0200

    Setup

--- public/js/employe/home.js
+++ public/js/employe/home.js
@@ -2,3 +2,11 @@ import { setHeader,requireAuthEmploye } from "../utils/index.js";
 
 requireAuthEmploye();
 setHeader();
+
+Promise.all([
+    fetch('/api/employe/current', {
+        headers: {
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+        }
+    }).then(res => res.json())
+])
