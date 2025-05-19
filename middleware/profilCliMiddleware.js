cb7c89e099707fb3eec12dc823f6e73412b51400
L0psun <evan.heulot@gmail.com>
2025-05-19 15:30:32 +0200

    Création de la première fonction de profilCliMiddleware.js

--- middleware/profilCliMiddleware.js
+++ middleware/profilCliMiddleware.js
@@ -1,8 +1,39 @@
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 const db = require('../db');
+const {response} = require("express");
+let id_banquier = null;
 
 dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
-function
+function verifierToken(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Token manquant ou invalide' });
+    }
+    const token = authHeader.split(' ')[1];
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.user = decoded;
+        // Récuper le banquier auquel est rattaché le client
+        if (req.user.type === 'banquier'){
+            db.get('SELECT banquier FROM client WHERE id = ?', [req.params.id_client], (err, row) => {
+                if (err) {
+                    return res.status(500).json({ error: err.message });
+                }
+                id_banquier = row.banquier;
+            })
+        }
+        if(req.user.type === 'client' && req.user.id === req.params.id_client || req.user.type === 'employe' && req.user.id === id_banquier){
+            next();
+        }else {
+            return res.status(403).json({ message: 'Accès refusé' });
+        }
+    }catch (err) {
+        if (err.name === 'TokenExpiredError') {
+            return res.status(401).json({ message: 'Token expiré' });
+        }
+        return res.status(401).json({ message: 'Token invalide' });
+    }
+}
