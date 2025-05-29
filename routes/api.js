30da0dc6f74f4d072570677b59c1ead5fd2c6d76
Nathan Le Fol <nathanlefol@hotmail.fr>
2025-05-29 16:02:57 +0200

    Add API stat

--- routes/api.js
+++ routes/api.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const authRoutes = require('./auth');
 const employeRoutes = require('./employe');
 const clientRoutes = require('./client');
+const statRoutes = require('./stat');
 
 // Monter les sous-routes
 router.use('/auth', authRoutes);
 router.use('/employe', employeRoutes);
 router.use('/client', clientRoutes);
+router.use('/stat', statRoutes);
 
 
 router.use((req, res) => {
---
+++ routes/stat.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+const db = require("../db");
+
+
+router.get('/nbClients', (req, res) => {
+    db.get('SELECT COUNT(*) AS nbClients FROM client', [], (err, row) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json({ nbClients: row.nbClients });
+    });
+});
+
+router.get('/nbEmployes', (req, res) => {
+    db.get('SELECT COUNT(*) AS nbEmployes FROM employe', [], (err, row) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json({ nbEmployes: row.nbEmployes });
+    });
+});
+
+router.get('/nbComptes', (req, res) => {
+    db.get('SELECT COUNT(*) AS nbComptes FROM compte', [], (err, row) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json({ nbComptes: row.nbComptes });
+    });
+});
+
+router.get('/nbOperations', (req, res) => {
+    db.get('SELECT (SELECT COUNT(*) FROM operation) + (SELECT COUNT(*) FROM virement) AS nbOperations', [], (err, row) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json({ nbOperations: row.nbOperations });
+    });
+});
+
+router.get('/nbCartes', (req, res) => {
+    db.get('SELECT COUNT(*) AS nbCartes FROM carte', [], (err, row) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json({ nbCartes: row.nbCartes });
+    });
+});
+
+router.get('/totalSolde', (req, res) => {
+    db.get('SELECT SUM(solde) AS totalSolde FROM compte', [], (err, row) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json({ totalSolde: row.totalSolde });
+    });
+});
+
+
+
+
+module.exports = router;
